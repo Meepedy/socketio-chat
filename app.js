aaff40e2df0a1ecbcfebd4883290cf00034ba759
Meepedy <david@psyon.org>
2018-05-14 15:31:13 -0400

    added const's for DOM stuff

--- app.js
+++ app.js
@@ -3,4 +3,16 @@ console.log('app.js loaded');
 
 const messageTypes = {LEFT: 'left', RIGHT: 'right', LOGIN: 'login'};
 
+//Chat Stuff
+const chatWindow = document.getElementById('chat');
+const messagesList = document.getElementById('messagesList');
+const messageInput = document.getElementById('messageInput');
+const sendBtn = document.getElementById('sendBtn');
+
+//Login Stuff
+let username = ''
+const usernameInput = document.getElementById('usernameInput');
+const loginBtn = document.getElementById('loginBtn');
+const loginWindow = document.getElementById('login');
+
 const messages = []; // {author, date, content, type}
